243481853ca3f91d61ef6b8b228027646aea4def
Nathan Michael McMillan <7141395+nathanmcmillan@users.noreply.github.com>
2023-05-21 08:41:43 -0400

    Develop (#2)
    * Oh no
    
    * Fixed compiler issue with tail calls
    
    * code hymn optimized, improved playground css
    
    * css-make.sh -> css-make.hm
    
    * LF
    
    ---------
    
    Co-authored-by: nmm-wsl <email>

---
+++ code/hymn_pattern.h
@@ -0,0 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef HYMN_PATTERN_LIB_H
+#define HYMN_PATTERN_LIB_H
+
+#include "hymn.h"
+
+void hymn_use_pattern(Hymn *H);
+
+#endif
